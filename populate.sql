557dca13bae7fc9b21dfd2c3220dfbde8b80b8e1
KHerashchenko <43710814+KHerashchenko@users.noreply.github.com>
2020-04-17 00:28:01 +0300

    Update populate.sql

--- populate.sql
+++ populate.sql
@@ -12,16 +12,16 @@ VALUES(2, 'rock');
 INSERT INTO Genres(genre_id, name)
 VALUES(3, 'indian pop');
 
-INSERT INTO Songs(song_id,title,artist_id,genre_id,beats_per_min,energy,danceability,loudness,liveness,valence,length,cousticness,speechiness)
-VALUES(1,'Childish',3,50,67,65,34,36,90,445,32,17);
-INSERT INTO Songs(song_id,title,artist_id,genre_id,beats_per_min,energy,danceability,loudness,liveness,valence,length,cousticness,speechiness)
-VALUES(2,'Palm City',2,54,57,75,65,32,90,235,23,11);
-INSERT INTO Songs(song_id,title,artist_id,genre_id,beats_per_min,energy,danceability,loudness,liveness,valence,length,cousticness,speechiness)
-VALUES(3,'a lot',2,0,37,634,35,32,33,445,43,37);
-INSERT INTO Songs(song_id,title,artist_id,genre_id,beats_per_min,energy,danceability,loudness,liveness,valence,length,cousticness,speechiness)
-VALUES(4,'Rockstar',1,54,27,23,34,45,90,345,352,45);
-INSERT INTO Songs(song_id,title,artist_id,genre_id,beats_per_min,energy,danceability,loudness,liveness,valence,length,cousticness,speechiness)
-VALUES(5,'Birds',2,50,13,34,24,35,455,56,19);
+INSERT INTO Songs(song_id,title,artist_id,genre_id,beats_per_min,energy,danceability,loudness,liveness,valence,length,acousticness,speechiness,popularity)
+VALUES(1,'Childish',3,50,67,65,34,36,90,445,32,17,98);
+INSERT INTO Songs(song_id,title,artist_id,genre_id,beats_per_min,energy,danceability,loudness,liveness,valence,length,acousticness,speechiness,popularity)
+VALUES(2,'Palm City',2,54,57,75,65,32,90,235,23,11,77);
+INSERT INTO Songs(song_id,title,artist_id,genre_id,beats_per_min,energy,danceability,loudness,liveness,valence,length,acousticness,speechiness,popularity)
+VALUES(3,'a lot',2,0,37,634,35,32,33,445,43,37,65);
+INSERT INTO Songs(song_id,title,artist_id,genre_id,beats_per_min,energy,danceability,loudness,liveness,valence,length,acousticness,speechiness,popularity)
+VALUES(4,'Rockstar',1,54,27,23,34,45,90,345,352,45,90);
+INSERT INTO Songs(song_id,title,artist_id,genre_id,beats_per_min,energy,danceability,loudness,liveness,valence,length,acousticness,speechiness,popularity)
+VALUES(5,'Birds',2,50,13,34,24,35,455,56,19,57);
 
 INSERT INTO SongsArtists(artist_id, song_id)
 VALUES(3, 3);
