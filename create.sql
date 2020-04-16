e7fab559c2a9195d38f88defe28a7bb04807aa0b
KHerashchenko <43710814+KHerashchenko@users.noreply.github.com>
2020-04-16 23:47:59 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -7,6 +7,8 @@ name char(20)
 ALTER TABLE Genres
 ADD CONSTRAINT genre_pk PRIMARY KEY (genre_id);
 
+-- 
+
 CREATE TABLE Artists
 (
 artist_id integer,
@@ -16,11 +18,12 @@ name char(20)
 ALTER TABLE Artists
 ADD CONSTRAINT artist_pk PRIMARY KEY (artist_id);
 
+-- 
+
 CREATE TABLE Songs
 (
 song_id integer,
 title char(20),
-artist_id integer,
 genre_id integer,
 beats_per_min integer,
 energy integer,
@@ -36,14 +39,19 @@ speechiness integer
 ALTER TABLE Songs
 ADD CONSTRAINT song_pk 
     PRIMARY KEY (song_id);
-
-ALTER TABLE Songs
-ADD CONSTRAINT song_artist_pk
-  FOREIGN KEY (artist_id)
-  REFERENCES Artists (artist_id);
-
+  
 ALTER TABLE Songs
 ADD CONSTRAINT song_genre_pk
   FOREIGN KEY (genre_id)
   REFERENCES Genres (genre_id);
+  
+-- 
 
+CREATE TABLE SongsArtists
+(
+artist_id integer,
+song_id integer
+);
+
+ALTER TABLE SongsArtists
+ADD CONSTRAINT song_artist_pk PRIMARY KEY (artist_id, song_id);
